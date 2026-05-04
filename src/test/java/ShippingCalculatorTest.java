64ac4c11a525e88ddd45b1e0c006a60fde96d032
hoang dung <dunghoangvolao123@gmail.com>
2026-05-04 15:47:56 +0700

    blyat

--- src/test/java/ShippingCalculatorTest.java
+++ src/test/java/ShippingCalculatorTest.java
@@ -1,4 +1,11 @@
 import org.junit.jupiter.api.Test;
+
+//error 3: Error:  Failed to execute goal org.apache.maven.plugins:maven-compiler-plugin:3.15.0:testCompile (default-testCompile) on project shipping-app: Compilation failure: Compilation failure: 
+//Error:  /home/runner/work/Bai10/Bai10/src/test/java/ShippingCalculatorTest.java:[6,5] cannot find symbol
+//Error:    symbol:   class ShippingCalculator 
+//fix: import this: 
+import com.lab.ShippingCalculator;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ShippingCalculatorTest {
